ee58993ec59ed18bbef24c80bdac194c86f65d3c
pramza427 <pramza427@gmail.com>
2024-05-20 14:54:57 -0500

    Fix audio changer colors
    Set colors on keybow to match up with which audio channels they are controlling

--- keybow_code.py
+++ keybow_code.py
@@ -1,11 +1,13 @@
-# Copy and paste this into the code.py file in the CIRCUITPY directory (Keybow)
-
 # There are three layers, selected by pressing and holding key 3 (top left), 
-# then tapping one of the coloured layer selector keys to the right of it
+# then tapping one of the colored layer selector keys to the right of it
 
-# The layer colours are as follows:
+# The layer colors are as follows:
 
-#  * layer 1: pink: F13 - F24 used for pycaw media controls and - = \ that I use for discord mute defen and overlay respectively
+#  * layer 1: used for pycaw media controls and - = \ that I use for discord mute defen and overlay respectively
+#       * pink: F13 - F15  Audio controls 1
+#       * blue: F18 - F18  Audio controls 2
+#       * purple: F19 - F21  Audio controls 3
+#       * yellow: F22 - F24  Audio controls 4
 #  * layer 2: blue: 0 - 9 keypad inputs and keypad - and x : used for quickbuys in games
 #  * layer 3: yellow: sends strings on each key press
 
@@ -33,7 +35,7 @@ layout = KeyboardLayoutUS(keyboard)
 # Set up consumer control (used to send media key presses)
 consumer_control = ConsumerControl(usb_hid.devices)
 
-# keycodes to be picked up by pycaw_on_startup.py for audio adjustments
+# keycodes to be picked up by pycaw_listener.py for audio adjustments
 # all buttons on this layer are prefixed by alt
 #       (L1) (L2) (L3)
 # Layer  -    =    \
@@ -61,10 +63,11 @@ layer_1 =     {0: Keycode.F13,
 
 # Layer with keypad 0-9, -, x 
 # Mainly used for quick buying in csgo
-# L L1 L2 L3
-# 7  8  9  X
-# 4  5  6  -
-# 1  2  3  0
+#   (L1)(L2)(L3)
+# L   -   =   \
+# 7   8   9   X
+# 4   5   6   -
+# 1   2   3   0
 layer_2 =     {0: Keycode.KEYPAD_ONE,
                4: Keycode.KEYPAD_TWO,
                8: Keycode.KEYPAD_THREE,
@@ -76,7 +79,10 @@ layer_2 =     {0: Keycode.KEYPAD_ONE,
                2: Keycode.KEYPAD_SEVEN,
                6: Keycode.KEYPAD_EIGHT,
                10: Keycode.KEYPAD_NINE,
-               14: Keycode.KEYPAD_ASTERISK 
+               14: Keycode.KEYPAD_ASTERISK,
+               7: Keycode.MINUS,
+               11: Keycode.EQUALS,
+               15: Keycode.BACKSLASH
                }
 
 
@@ -89,6 +95,7 @@ layer_3 =     {0: "pack ",
                5: "liquor ",
                9: "jugs "}
 
+# Define what keys handle each layer
 layers =      {7: layer_1,
                11: layer_2,
                15: layer_3}
@@ -103,16 +110,41 @@ selectors =   {7: keys[7],
 # Start on layer 1
 current_layer = 7
 
-# The colours for each layer
-colours = {7: (255, 0, 255),
-           11: (0, 255, 255),
-           15: (255, 255, 0)}
+audio_dial_colors = [(255, 89, 94),
+                     (25, 130, 255),
+                     (106, 76, 255),
+                     (138, 255, 38)]
+
+layer_7_colors = {0: audio_dial_colors[0],
+                  1: audio_dial_colors[0],
+                  2: audio_dial_colors[0],
+                  3: audio_dial_colors[0],
+                  4: audio_dial_colors[1],
+                  5: audio_dial_colors[1],
+                  6: audio_dial_colors[1],
+                  7: audio_dial_colors[1],
+                  8: audio_dial_colors[2],
+                  9: audio_dial_colors[2],
+                  10: audio_dial_colors[2],
+                  11: audio_dial_colors[2],
+                  12: audio_dial_colors[3],
+                  13: audio_dial_colors[3],
+                  14: audio_dial_colors[3],
+                  15: audio_dial_colors[3]}
+
+# The colors for each layer
+colors = {7: (255, 0, 255),
+          11: (0, 255, 255),
+          15: (255, 255, 0)}
 
 layer_keys = [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14]
 
 # Set the LEDs for each key in the current layer
 for k in layers[current_layer].keys():
-    keys[k].set_led(*colours[current_layer])
+    if current_layer == 7:
+        keys[k].set_led(*layer_7_colors[k])
+    else:
+        keys[k].set_led(*colors[current_layer])
 
 # To prevent the strings (as opposed to single key presses) that are sent from 
 # refiring on a single key press, the debounce time for the strings has to be 
@@ -133,7 +165,7 @@ while True:
 
         # If the modifier key is held, light up the layer selector keys
         for layer in layers.keys():
-            keys[layer].set_led(*colours[layer])
+            keys[layer].set_led(*colors[layer])
 
             # Change layer if layer key is pressed
             if current_layer != layer:
@@ -145,7 +177,10 @@ while True:
                         keys[k].set_led(0, 0, 0)
 
                     for k in layers[layer].keys():
-                        keys[k].set_led(*colours[layer])
+                        if current_layer == 7:
+                            keys[k].set_led(*layer_7_colors[k])
+                        else:
+                            keys[k].set_led(*colors[layer])
 
     # Turn off the layer selector LEDs if the modifier isn't held
     else:
@@ -170,13 +205,13 @@ while True:
                 # Volume Controls
                 if current_layer == 7:
                     debounce = long_debounce
-                    keyboard.send(Keycode.LEFT_ALT, key_press)
+                    keyboard.send(Keycode.RIGHT_ALT, key_press)
                     
                 # Keypad
                 elif current_layer == 11:
                     debounce = long_debounce
                     keyboard.send(key_press)
-
+                    
                 # Strings
                 elif current_layer == 15:
                     debounce = short_debounce
